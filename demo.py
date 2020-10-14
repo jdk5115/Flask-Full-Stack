5e3b6208b83d424c2d198d742b8e7f2ecbb1185b
Jonathan Kelly <jdk5115@gmail.com>
2020-10-14 14:05:31 -0400

    1

--- demo.py
+++ demo.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def home():
-    return ('Hello World!!!'), 200
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port= 8000, debug= True)
+    app.run(port= 5000, debug= True)
